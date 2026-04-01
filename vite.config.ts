0a61728de7fcdb7a8f1ea1ed2e4ddc935e7703bb
Syntek Honduras <syntekhonduras@gmail.com>
2026-03-31 18:12:00 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/',
+  base: '/syntek-honduras-hero/',
   server: {
     host: "::",
     port: 8080,
